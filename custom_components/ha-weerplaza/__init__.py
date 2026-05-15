b72f61a70102ec97736678dc460cb1f00d4fcb03
TranQuiL <90527254+malosaaa@users.noreply.github.com>
2026-05-15 19:02:39 +0200

    Refactor import statements in __init__.py

--- custom_components/ha-weerplaza/__init__.py
+++ custom_components/ha-weerplaza/__init__.py
@@ -2,7 +2,7 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.const import Platform
-from .const import DOMAIN, 
+from .const import DOMAIN
 from .coordinator import WeerplazaCoordinator
 from .cache import PersistentCache
 
